e6d95387bd3dedefe676f8cffcadf1c2d12f1380
NIKO <nicolas.mmb@hotmail.com>
2026-01-10 10:12:57 -0300

    Format metadata in manifest.py for improved readability

--- manifest.py
+++ manifest.py
@@ -1,5 +1,7 @@
 # Manifest for MicroServer so it can be installed via mip
-metadata(version="0.1.0", description="Minimal HTTP/WebSocket microserver for MicroPython")
+metadata(
+    version="0.1.0", description="Minimal HTTP/WebSocket microserver for MicroPython"
+)
 
 # Install modules directly into /lib so imports stay the same
 module("microserver.py")
